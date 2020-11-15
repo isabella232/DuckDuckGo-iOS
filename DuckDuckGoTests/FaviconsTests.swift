7c1b2cab45eb9e013c23912f71d7709ce04fcdfd
Gábor Szakács <gaborszakacs@users.noreply.github.com>
2020-11-15 18:38:58 +0100

    Update FaviconsTests.swift

--- DuckDuckGoTests/FaviconsTests.swift
+++ DuckDuckGoTests/FaviconsTests.swift
@@ -100,6 +100,10 @@ class FaviconsTests: XCTestCase {
         
         let resource = Favicons.shared.defaultResource(forDomain: "example.com")
         XCTAssertEqual(resource?.cacheKey, "\(Favicons.Constants.salt)example.com".sha256())
+        
+        
+        
+        
         XCTAssertEqual(resource?.downloadURL, URL(string: "https://example.com/apple-touch-iconnn.png"))
         
     }
